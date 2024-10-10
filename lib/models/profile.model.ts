02f6ccea7207c5057960f6ccb1f667da1a545505
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-10 15:38:14 -0600

    feat(profile.models.ts): append the servers mongoose Server model reference array property

--- lib/models/profile.model.ts
+++ lib/models/profile.model.ts
@@ -6,10 +6,12 @@ const profileSchema = new mongoose.Schema({
   name: { type: String, required: true },
   imageUrl: { type: String, required: true },
   email: { type: String, required: true },
+  servers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Server" }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-const Profile = mongoose.model("Profile", profileSchema);
+const Profile =
+  mongoose.models.Profile || mongoose.model("Profile", profileSchema);
 
 export default Profile;
