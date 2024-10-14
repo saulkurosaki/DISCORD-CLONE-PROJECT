fab71e9c717a285f9b35292b32660c6eadfef315
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-14 16:57:26 -0600

    feat(initial-modal.tsx): implement the DialogDescription component

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -15,6 +15,10 @@ const InitialModal = () => {
           <DialogTitle className="text-2xl text-center font-bold">
             Customize your server
           </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
