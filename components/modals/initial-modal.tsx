d74499654234acfcc408edcc32da37a538946134
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-14 16:55:03 -0600

    feat(initial-modal.tsx): implement shadcn Dialog component along all its imports and implement the initial DialogTitle component

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 const InitialModal = () => {
-  return <div>InitialModal</div>;
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default InitialModal;
