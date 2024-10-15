e46bcaee4c1087e982d81f125bc2574db4a09028
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-15 00:05:23 -0600

    feat(FileUpload): implement the FileUploadProps interface along as the component accepted values

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,4 +1,16 @@
-const FileUpload = () => {
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+
+const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
   return <div>FileUpload</div>;
 };
 
