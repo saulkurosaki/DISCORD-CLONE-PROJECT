ce822dd377883d0a50832bdfedbf4978cb4972ea
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-15 00:28:39 -0600

    feat(FileUpload): implement the delete image x button

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -20,6 +20,13 @@ const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
     return (
       <div className="relative w-20 h-20">
         <Image fill src={value} alt="Uploaded image" className="rounded-full" />
+        <button
+          type="button"
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+        >
+          <X className="w-4 h-4" />
+        </button>
       </div>
     );
   }
