52c1b320795e7ee317ce83fed5b4d1c27f7abc6b
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-15 13:58:58 -0600

    feat(InitialModal): implement the complete onSubmit function to create a new Server in the Db

--- components/modals/InitialModal.tsx
+++ components/modals/InitialModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -25,6 +26,7 @@ import { Button } from "@/components/ui/button";
 
 import { useEffect, useState } from "react";
 import FileUpload from "@/components/FileUpload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Server name is required." }),
@@ -32,6 +34,7 @@ const formSchema = z.object({
 });
 
 const InitialModal = () => {
+  const router = useRouter();
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -49,7 +52,15 @@ const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!isMounted) {
