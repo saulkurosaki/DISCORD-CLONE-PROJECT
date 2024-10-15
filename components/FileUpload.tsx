d9940fb0a4e9377ef79fa4e5b2b1ee7cf5426b52
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-15 00:18:00 -0600

    feat(FileUpload): implement the UploadDropzone component within the main one along its necessary properties

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -11,7 +11,17 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  return <div>FileUpload</div>;
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
 };
 
 export default FileUpload;
