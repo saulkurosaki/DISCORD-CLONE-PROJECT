2d0b0018b84b080f2e9187f25127a56d9ca3812e
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-15 14:18:30 -0600

    feat(NavigationSidebar): implement the component to get the current profile along the servers asociated to that current profile

--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -1,5 +1,30 @@
-const NavigationSidebar = () => {
-  return <div>NavigationSidebar</div>;
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+      Navigation sidebar
+    </div>
+  );
 };
 
 export default NavigationSidebar;
