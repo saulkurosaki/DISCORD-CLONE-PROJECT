50c92c76c57a2ec31324c5df53c960f8a4287695
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-16 15:02:08 -0600

    feat(NavigationSidebar): implement the ModeToggle and UserButton components within the main component

--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -1,10 +1,13 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
+import { ModeToggle } from "../ModeToggle";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import NavigationAction from "./NavigationAction";
 import { Separator } from "../ui/separator";
 import { ScrollArea } from "../ui/scroll-area";
+
+import NavigationAction from "./NavigationAction";
 import NavigationItem from "./NavigationItem";
 
 const NavigationSidebar = async () => {
@@ -27,7 +30,9 @@ const NavigationSidebar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
+
       <Separator className="h-2px bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+
       <ScrollArea className="flex-1 w-full">
         {servers.map((server) => (
           <div key={server.id} className="mb-4">
@@ -39,6 +44,18 @@ const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
+
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
