a083dffa128512843c8f331cea67af9fee34783f
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-16 14:31:34 -0600

    feat(NavigationItem): implement the NavigationItem component initial layout

---
+++ components/navigation/NavigationItem.tsx
@@ -0,0 +1,5 @@
+const NavigationItem = () => {
+  return <div>NavigationItem</div>;
+};
+
+export default NavigationItem;
--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -4,6 +4,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import NavigationAction from "./NavigationAction";
 import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -26,6 +27,13 @@ const NavigationSidebar = async () => {
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
       <Separator className="h-2px bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            {server.name}
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
