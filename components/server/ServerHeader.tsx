9a3b9966f0709aa3c82c769c25213dee422e678a
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-17 17:10:55 -0600

    feat(ServerHeader): implement the create channel button only for moderators

--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -53,12 +53,12 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             <Users className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
-        {/* {isModerator && (
+        {isModerator && (
           <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
-        )} */}
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
