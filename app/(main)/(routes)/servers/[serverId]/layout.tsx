43a4b937fe2b8df6a8f949fe45c1cc6414dedce3
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-17 16:10:50 -0600

    feat(ServerIdLayout): implement the profile and server based on the url params serverId fetching along their no existing cases handlers

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,5 +1,37 @@
-const ServerIdLayout = () => {
-  return <div>ServerIdLayout</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const ServerIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  return <div>{children}</div>;
 };
 
 export default ServerIdLayout;
