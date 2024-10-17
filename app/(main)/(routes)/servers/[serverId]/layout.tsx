c381b62173fe30f2014b21361907ad20972ccaf1
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-17 16:32:15 -0600

    feat(ServerSidebar): implement the individual profile fetching

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,3 +1,4 @@
+import ServerSidebar from "@/components/server/ServerSidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs/server";
@@ -34,7 +35,7 @@ const ServerIdLayout = async ({
   return (
     <div className="h-full">
       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        Server Sidebar
+        <ServerSidebar serverId={params.serverId} />
       </div>
       <main className="h-full md:pl-60">{children}</main>
     </div>
