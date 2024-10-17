27475034f93d2ca455d0e3aed8895f2541437ce3
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-17 16:17:10 -0600

    feat(ServerSidebar): implement the ServerSidebar component initial layout

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -31,7 +31,14 @@ const ServerIdLayout = async ({
     return redirect("/");
   }
 
-  return <div>{children}</div>;
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        Server Sidebar
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
 };
 
 export default ServerIdLayout;
