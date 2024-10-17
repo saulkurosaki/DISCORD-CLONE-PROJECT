29cde9768640b13fae319fce6fe802dcae4c07b0
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-17 17:16:27 -0600

    feat(ServerHeader): implement the leave server button for everyone but admins

--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -11,6 +11,7 @@ import {
 } from "../ui/dropdown-menu";
 import {
   ChevronDown,
+  LogOut,
   PlusCircle,
   Settings,
   Trash,
@@ -66,6 +67,12 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
             Delete Server
             <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
