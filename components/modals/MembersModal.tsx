911b6f7625552651a9f8ed91ef9cf9811a2331dc
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-19 09:39:28 -0600

    feat(MemberModal): implement the member profile role badge and email display

--- components/modals/MembersModal.tsx
+++ components/modals/MembersModal.tsx
@@ -12,6 +12,13 @@ import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ScrollArea } from "../ui/scroll-area";
 import UserAvatar from "../UserAvatar";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
+};
 
 const MembersModal = () => {
   const { onOpen, isOpen, onClose, type, data } = useModal();
@@ -38,7 +45,9 @@ const MembersModal = () => {
               <div className="flex flex-col gap-y-1">
                 <div className="text-xs font-semibold flex items-center">
                   {member.profile.name}
+                  {roleIconMap[member.role]}
                 </div>
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
               </div>
             </div>
           ))}
