f7e067881b0ffe343c56b0955d6bddb543c1ea9d
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-21 12:17:07 -0600

    feat(LeaveServerModal): implement the modal title along the modal description

--- components/modals/LeaveServerModal.tsx
+++ components/modals/LeaveServerModal.tsx
@@ -3,6 +3,7 @@
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
@@ -18,7 +19,6 @@ import axios from "axios";
 
 const LeaveServerModal = () => {
   const { onOpen, isOpen, onClose, type, data } = useModal();
-  const origin = useOrigin();
 
   const isModalOpen = isOpen && type === "leaveServer";
   const { server } = data;
@@ -26,71 +26,23 @@ const LeaveServerModal = () => {
   const [copied, setCopied] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
-
-  const onCopy = () => {
-    navigator.clipboard.writeText(inviteUrl);
-    setCopied(true);
-
-    setTimeout(() => {
-      setCopied(false);
-    }, 1000);
-  };
-
-  const onNew = async () => {
-    try {
-      setIsLoading(true);
-
-      const response = await axios.patch(
-        `/api/servers/${server?.id}/invite-code`
-      );
-
-      onOpen("invite", { server: response.data });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Invite Friends
+            Leave Server
           </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to leave{" "}
+            <span className="font-semibold text-indigo-500">
+              {server?.name}
+            </span>
+            ?
+          </DialogDescription>
         </DialogHeader>
 
-        <div className="p-6">
-          <Label className="uppercase text-sx font-bold text-zinc-500 dark:text-secondary/70">
-            Server invite link
-          </Label>
-          <div className="flex items-center mt-2 gap-x-2">
-            <Input
-              disabled={isLoading}
-              className="bg-zinc-300/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-black"
-              value={inviteUrl}
-            />
-            <Button disabled={isLoading} onClick={onCopy} size="icon">
-              {copied ? (
-                <Check className="w-4 h-4" />
-              ) : (
-                <Copy className="w-4 h-4" />
-              )}
-            </Button>
-          </div>
-          <Button
-            onClick={onNew}
-            disabled={isLoading}
-            variant="link"
-            size="sm"
-            className="text-xs text-zing-500 mt-4"
-          >
-            Generate a new link
-            <RefreshCw className="w-4 h-4 ml-2" />
-          </Button>
-        </div>
+        <div className="p-6">Leave Server</div>
       </DialogContent>
     </Dialog>
   );
