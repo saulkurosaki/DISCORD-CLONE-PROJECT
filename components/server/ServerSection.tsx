3f2e441a40a42734f62c641b897b105ed98b5ff3
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-24 19:38:58 -0600

    feat(ServerSection): implement the onOpen useModal custom hook function to open the create channel modal when the button is pressed

--- components/server/ServerSection.tsx
+++ components/server/ServerSection.tsx
@@ -4,6 +4,7 @@ import { ServerWithMembersWithProfiles } from "@/types";
 import { ChannelType, MemberRole } from "@prisma/client";
 import ActionTooltip from "../ActionTooltip";
 import { Plus } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerSectionProps {
   label: string;
@@ -20,6 +21,8 @@ const ServerSection = ({
   channelType,
   server,
 }: ServerSectionProps) => {
+  const { onOpen } = useModal();
+
   return (
     <div className="flex items-center justify-between py-2">
       <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
@@ -27,7 +30,10 @@ const ServerSection = ({
       </p>
       {role !== MemberRole.GUEST && sectionType === "channels" && (
         <ActionTooltip label="Create Channel" side="top">
-          <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
+          <button
+            onClick={() => onOpen("createChannel")}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          >
             <Plus className="w-4 h-4" />
           </button>
         </ActionTooltip>
