50a0a9193bcb35c5fa16a945da5851ca690bdd50
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-24 22:55:16 -0600

    feat(ServerChannel): implement the edit button with its tooltip for those channels that are not general

--- components/server/ServerChannel.tsx
+++ components/server/ServerChannel.tsx
@@ -2,8 +2,9 @@
 
 import { cn } from "@/lib/utils";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
-import { Hash, Mic, Video } from "lucide-react";
+import { Edit, Hash, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import ActionTooltip from "../ActionTooltip";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -41,6 +42,17 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
       >
         {channel.name}
       </p>
+      {channel.name !== "general" && role !== MemberRole.GUEST && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <ActionTooltip label="Edit">
+            <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </ActionTooltip>
+
+          {/* <ActionTooltip label="Delete">
+            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </ActionTooltip> */}
+        </div>
+      )}
     </button>
   );
 };
