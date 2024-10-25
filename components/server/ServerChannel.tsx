7ddb9754beb0004c574e8f64dd7ebcf93b3f0a75
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-24 22:48:50 -0600

    feat(ServerChannel): implement the complete text channels display along all of its dynamic classnames

--- components/server/ServerChannel.tsx
+++ components/server/ServerChannel.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Hash, Mic, Video } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -16,7 +18,31 @@ const iconMap = {
 };
 
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
-  return <div>ServerChannel</div>;
+  const router = useRouter();
+  const params = useParams();
+
+  const Icon = iconMap[channel.type];
+
+  return (
+    <button
+      onClick={() => {}}
+      className={cn(
+        "group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
+      )}
+    >
+      <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
+      <p
+        className={cn(
+          "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+          params?.channelId === channel.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}
+      >
+        {channel.name}
+      </p>
+    </button>
+  );
 };
 
 export default ServerChannel;
