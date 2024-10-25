bf00835e7014a28149629fbc70371cc77bf40cdc
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-24 19:35:27 -0600

    feat(ServerSection): implement the moderator or admin create text channels section button

--- components/server/ServerSection.tsx
+++ components/server/ServerSection.tsx
@@ -2,6 +2,8 @@
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ChannelType, MemberRole } from "@prisma/client";
+import ActionTooltip from "../ActionTooltip";
+import { Plus } from "lucide-react";
 
 interface ServerSectionProps {
   label: string;
@@ -18,7 +20,20 @@ const ServerSection = ({
   channelType,
   server,
 }: ServerSectionProps) => {
-  return <div>ServerSection</div>;
+  return (
+    <div className="flex items-center justify-between py-2">
+      <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+        {label}
+      </p>
+      {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionTooltip label="Create Channel" side="top">
+          <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
+            <Plus className="w-4 h-4" />
+          </button>
+        </ActionTooltip>
+      )}
+    </div>
+  );
 };
 
 export default ServerSection;
