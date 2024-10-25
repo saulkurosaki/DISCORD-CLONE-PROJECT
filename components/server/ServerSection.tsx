8dae15e2521b8c0f3c852d05a79aec1817322c4b
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-25 08:21:14 -0600

    fix(ServerSection): fix little action tooltip error

--- components/server/ServerSection.tsx
+++ components/server/ServerSection.tsx
@@ -38,8 +38,8 @@ const ServerSection = ({
           </button>
         </ActionTooltip>
       )}
-      {role !== MemberRole.ADMIN && sectionType === "members" && (
-        <ActionTooltip label="Members" side="top">
+      {role === MemberRole.ADMIN && sectionType === "members" && (
+        <ActionTooltip label="Manage Members" side="top">
           <button
             onClick={() => onOpen("members", { server })}
             className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
