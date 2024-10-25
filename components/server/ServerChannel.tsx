68d96c485c5510acee4797464bf4e216bed9bc27
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-25 08:02:31 -0600

    feat(ServerChannel): implement the lock icon only for the general channels

--- components/server/ServerChannel.tsx
+++ components/server/ServerChannel.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
-import { Edit, Hash, Mic, Trash, Video } from "lucide-react";
+import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import ActionTooltip from "../ActionTooltip";
 
@@ -52,6 +52,9 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
             <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
         </div>
+      )}
+      {channel.name === "general" && (
+        <Lock className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" />
       )}
     </button>
   );
