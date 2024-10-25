2bf1f13ee2ebbd03cf9461161da96da9e98ff9ba
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-25 08:08:03 -0600

    feat(ServerSidebar): implement the complete video channels server section and server channels

--- components/server/ServerSidebar.tsx
+++ components/server/ServerSidebar.tsx
@@ -163,6 +163,25 @@ const ServerSidebar = async ({ serverId }: ServerSidebarParams) => {
             ))}
           </div>
         )}
+
+        {!!videoChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.VIDEO}
+              role={role}
+              label="Video Channels"
+            />
+            {videoChannels.map((channel) => (
+              <ServerChannel
+                key={channel.id}
+                channel={channel}
+                role={role}
+                server={server}
+              />
+            ))}
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
