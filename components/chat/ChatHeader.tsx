1b483c0f950c7647e89e706bf0514a1907fb5574
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-26 13:08:32 -0600

    feat(MobileToggle): implement the MobileToggle component initial layout

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -1,4 +1,5 @@
 import { Hash, Menu } from "lucide-react";
+import MobileToggle from "../MobileToggle";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -10,7 +11,7 @@ interface ChatHeaderProps {
 const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div className="font-semibold text-md px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <Menu />
+      <MobileToggle />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
