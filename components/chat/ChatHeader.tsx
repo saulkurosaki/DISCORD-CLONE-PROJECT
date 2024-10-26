1f4f5ea2e082934e0974f33c982f218907773110
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-26 13:05:48 -0600

    feat(ChatHeader): implement the not functional hamburguer menu button along channel name and type icon

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -1,5 +1,22 @@
-const ChatHeader = () => {
-  return <div>ChatHeader</div>;
+import { Hash, Menu } from "lucide-react";
+
+interface ChatHeaderProps {
+  serverId: string;
+  name: string;
+  type: "channel" | "conversation";
+  imageUrl?: string;
+}
+
+const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
+  return (
+    <div className="font-semibold text-md px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+      <Menu />
+      {type === "channel" && (
+        <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+    </div>
+  );
 };
 
 export default ChatHeader;
