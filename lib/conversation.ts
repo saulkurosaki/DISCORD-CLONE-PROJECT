e514051993a9283c25c2a1d1e8b25afb420cf905
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-27 16:23:48 -0600

    feat(lib/conversation.ts) implement the complete getOrCreateConversation async function to check if there is an existing conversation before creating it

--- lib/conversation.ts
+++ lib/conversation.ts
@@ -1,5 +1,20 @@
 import { db } from "./db";
 
+export const getOrCreateConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  let conversation =
+    (await findConversation(memberOneId, memberTwoId)) ||
+    (await findConversation(memberTwoId, memberOneId));
+
+  if (!conversation) {
+    conversation = await createNewConversation(memberOneId, memberTwoId);
+  }
+
+  return conversation;
+};
+
 const findConversation = async (memberOneId: string, memberTwoId: string) => {
   try {
     return await db.conversation.findFirst({
