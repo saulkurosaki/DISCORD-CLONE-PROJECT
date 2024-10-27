ef662777c9bceb68e586d65fbd5c148128239d77
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-27 16:19:36 -0600

    feat(lib/conversation.ts) implement the complete createNewConversation async function to create a specific conversation

--- lib/conversation.ts
+++ lib/conversation.ts
@@ -1,21 +1,53 @@
 import { db } from "./db";
 
 const findConversation = async (memberOneId: string, memberTwoId: string) => {
-  return await db.conversation.findFirst({
-    where: {
-      AND: [{ memberOneId: memberOneId }, { memberTwoId: memberTwoId }],
-    },
-    include: {
-      memberOne: {
-        include: {
-          profile: true,
+  try {
+    return await db.conversation.findFirst({
+      where: {
+        AND: [{ memberOneId: memberOneId }, { memberTwoId: memberTwoId }],
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
         },
       },
-      memberTwo: {
-        include: {
-          profile: true,
+    });
+  } catch {
+    return null;
+  }
+};
+
+const createNewConversation = async (
+  memberOneId: string,
+  memberTwoId: string
+) => {
+  try {
+    return await db.conversation.create({
+      data: {
+        memberOneId,
+        memberTwoId,
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
         },
       },
-    },
-  });
+    });
+  } catch {
+    return null;
+  }
 };
