5eaf70318fbbf494a8a103384708aa43e2c5a7a3
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-29 15:25:37 -0600

    feat(RootLayout): implement the SocketProvider within the RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { cn } from "@/lib/utils";
 import ModalProvider from "@/components/providers/ModalProvider";
+import SocketProvider from "@/components/providers/SocketProvider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -29,8 +30,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
