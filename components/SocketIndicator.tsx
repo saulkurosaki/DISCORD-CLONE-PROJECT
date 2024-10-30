eeda300048acc396ade9f77fbeabd96f4318e8a3
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-29 19:39:22 -0600

    feat(SocketIndicator): implement the complete SocketIndicator component

--- components/SocketIndicator.tsx
+++ components/SocketIndicator.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { useSocket } from "./providers/SocketProvider";
+import { Badge } from "./ui/badge";
+
 const SocketIndicator = () => {
-  return <div>SocketIndicator</div>;
+  const { isConnected } = useSocket();
+
+  if (!isConnected) {
+    return (
+      <Badge variant="outline" className="bg-yellow-600 text-white border-none">
+        Fallback: Polling every 1 second
+      </Badge>
+    );
+  }
+
+  return (
+    <Badge variant="outline" className="bg-emerald-600 text-white border-none">
+      Live: Real-time updates
+    </Badge>
+  );
 };
 
 export default SocketIndicator;
