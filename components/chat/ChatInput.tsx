09182e457127b8accf5d37768b6bfbf3beaf2b17
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-29 19:47:13 -0600

    feat(ChatInput): implement the ChatInputProps interface along the formSchema zod validation schema

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -1,4 +1,19 @@
-const ChatInput = () => {
+"use client";
+
+import * as z from "zod";
+
+interface ChatInputProps {
+  apiUrl: string;
+  query: Record<string, any>;
+  name: string;
+  type: "conversation" | "channel";
+}
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+
+const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   return <div>ChatInput</div>;
 };
 
