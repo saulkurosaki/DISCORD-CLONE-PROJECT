80eac71bc09d81777c09c60c7332b4771eddefa4
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-29 20:02:06 -0600

    feat(ChatInput): implement the complete zod and react-hook-form form states

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Form, FormControl, FormField, FormItem } from "../ui/form";
+import { Input } from "../ui/input";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -14,6 +19,19 @@ const formSchema = z.object({
 });
 
 const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = (value: z.infer<typeof formSchema>) => {
+    console.log(value);
+  };
+
   return <div>ChatInput</div>;
 };
 
