dc5f9e4edfeee97fa45d0b581df3177a67eeca3a
saulkurosaki <saulkurosaki13@gmail.com>
2024-11-01 14:20:56 -0600

    feat(QueryProvider): implement the complete QueryProvider component

--- components/providers/QueryProvider.tsx
+++ components/providers/QueryProvider.tsx
@@ -1,7 +1,14 @@
 "use client";
 
-const QueryProvider = () => {
-  return <div>QueryProvider</div>;
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 };
 
 export default QueryProvider;
