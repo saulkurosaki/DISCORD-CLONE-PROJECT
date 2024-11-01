b6b14997f61618c7ce3dfa6f6291cfdb265da7b7
saulkurosaki <saulkurosaki13@gmail.com>
2024-11-01 13:50:28 -0600

    feat(ChatMessages): implement the complete ChatMessagesProps interface

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -1,6 +1,28 @@
 "use client";
 
-const ChatMessages = () => {
+interface ChatMessagesProps {
+  name: string;
+  member: string;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  type: "channel" | "conversation";
+}
+
+const ChatMessages = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessagesProps) => {
   return <div>ChatMessages</div>;
 };
 
