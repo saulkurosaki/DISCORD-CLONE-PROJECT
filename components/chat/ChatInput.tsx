4af52c673c0c9bd39eb190f15941de36617110ae
saulkurosaki <saulkurosaki13@gmail.com>
2024-11-01 13:36:16 -0600

    feat(ChatInput): implement the form reset after the send

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -11,6 +11,7 @@ import { Input } from "../ui/input";
 import { Plus, Smile } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
 import EmojiPicker from "../EmojiPicker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -25,6 +26,7 @@ const formSchema = z.object({
 
 const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   const { onOpen } = useModal();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -43,6 +45,9 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
       });
 
       await axios.post(url, values);
+
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
