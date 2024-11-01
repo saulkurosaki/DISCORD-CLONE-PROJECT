94c78317d5b9061c8096959fb8d8b049dc65cdc3
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-31 19:40:45 -0600

    feat(FileUpload): implement the complete pdf upload management display ui

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
+import { useState } from "react";
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
@@ -11,9 +12,9 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  const fileType = value?.split(".").pop();
+  const [fileType, setFileType] = useState("");
 
-  if (value && fileType !== "pdf") {
+  if (value && fileType !== "application/pdf") {
     return (
       <div className="relative w-20 h-20">
         <Image fill src={value} alt="Uploaded image" className="rounded-full" />
@@ -28,10 +29,34 @@ const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
     );
   }
 
+  if (value && fileType === "application/pdf") {
+    return (
+      <div className="max-w-[462px] max-h-[250px] relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="w-10 h-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="truncate ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          type="button"
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+        >
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    );
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
+        setFileType(res?.[0].type);
         onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
