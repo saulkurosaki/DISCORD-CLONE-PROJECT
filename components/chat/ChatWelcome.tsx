a1c331b913d7f9392063f6dbae3bc96f35eb7706
saulkurosaki <saulkurosaki13@gmail.com>
2024-11-01 14:09:08 -0600

    feat(ChatWelcome): implement the complete ChatWelcome component

--- components/chat/ChatWelcome.tsx
+++ components/chat/ChatWelcome.tsx
@@ -1,7 +1,29 @@
-"use client";
+import { Hash } from "lucide-react";
 
-const ChatWelcome = () => {
-  return <div>ChatWelcome</div>;
+interface ChatWelcomeProps {
+  name: string;
+  type: "channel" | "conversation";
+}
+
+const ChatWelcome = ({ name, type }: ChatWelcomeProps) => {
+  return (
+    <div className="space-y-2 px-4 mb-4">
+      {type === "channel" && (
+        <div className="w-[75px] h-[75px] rounded-full bg-zinc-500 dark:bg-zinc-700 flex items-center justify-center">
+          <Hash className="h-12 w-12 text-white" />
+        </div>
+      )}
+      <p className="text-xl md:text-3xl font-bold">
+        {type === "channel" ? "Welcome to #" : ""}
+        {name}
+      </p>
+      <p className="text-zinc-600 dark:text-zinc-400 text-sm">
+        {type === "channel"
+          ? `This is the start of the #${name} channel.`
+          : `This is the start of your conversation with ${name}`}
+      </p>
+    </div>
+  );
 };
 
 export default ChatWelcome;
