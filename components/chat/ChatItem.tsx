fe87d220e7c5517c1f868fe99debc5933ce63365
saulkurosaki <saulkurosaki13@gmail.com>
2024-11-04 15:06:20 -0600

    feat(ChatItem): implement the complete ChatItemProps interface

--- components/chat/ChatItem.tsx
+++ components/chat/ChatItem.tsx
@@ -1,6 +1,34 @@
 "use client";
 
-const ChatItem = () => {
+import { Member, Profile } from "@prisma/client";
+
+interface ChatItemProps {
+  id: string;
+  content: string;
+  member: Member & {
+    profile: Profile;
+  };
+  timestamp: string;
+  fileUrl: string | null;
+  deleted: boolean;
+  currentMember: Member;
+  isUpdated: boolean;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+}
+
+const ChatItem = ({
+  id,
+  content,
+  member,
+  timestamp,
+  fileUrl,
+  deleted,
+  currentMember,
+  isUpdated,
+  socketUrl,
+  socketQuery,
+}: ChatItemProps) => {
   return <div>ChatItem</div>;
 };
 
