48a75d88db99b9da47f86e8c168a7a1cef49b0d7
saulkurosaki <saulkurosaki13@gmail.com>
2024-11-08 13:45:47 -0600

    feat(DeleteMessageModal): implement the complete DeleteMessageModal component along the complete delete message feature

--- components/modals/DeleteMessageModal.tsx
+++ components/modals/DeleteMessageModal.tsx
@@ -15,14 +15,12 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "../ui/button";
 import { useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
 
 const DeleteMessageModal = () => {
   const { isOpen, onClose, type, data } = useModal();
-  const router = useRouter();
 
   const isModalOpen = isOpen && type === "deleteMessage";
-  const { server, channel } = data;
+  const { apiUrl, query } = data;
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -30,18 +28,13 @@ const DeleteMessageModal = () => {
     try {
       setIsLoading(true);
       const url = qs.stringifyUrl({
-        url: `/api/channels/${channel?.id}`,
-        query: {
-          serverId: server?.id,
-        },
+        url: apiUrl || "",
+        query,
       });
 
       await axios.delete(url);
 
       onClose();
-      router.refresh();
-      router.push(`/servers/${server?.id}`);
-      router.refresh();
     } catch (error) {
       console.log(error);
     } finally {
@@ -54,14 +47,11 @@ const DeleteMessageModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Delete Channel
+            Delete Message
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
             Are you sure you want to do this? <br />
-            <span className="font-semibold text-indigo-500">
-              #{channel?.name}
-            </span>{" "}
-            will be permanently deleted.
+            This message will be permanently deleted.
           </DialogDescription>
         </DialogHeader>
 
