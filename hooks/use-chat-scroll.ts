97b48eeb4a95fd7d1a94a58ed51445d4d2422f5d
saulkurosaki <saulkurosaki13@gmail.com>
2024-11-10 19:24:58 -0600

    feat(use-chat-scroll.ts): implement the useChatScroll custom hook new message bottom auto scroll use effect

--- hooks/use-chat-scroll.ts
+++ hooks/use-chat-scroll.ts
@@ -34,4 +34,32 @@ export const useChatScroll = ({
       topDiv?.removeEventListener("scroll", handleScroll);
     };
   }, [shouldLoadMore, loadMore, chatRef]);
+
+  useEffect(() => {
+    const bottomDiv = bottomRef?.current;
+    const topDiv = chatRef?.current;
+
+    const shouldAutoScroll = () => {
+      if (!hasInitialized && bottomDiv) {
+        setHasInitialized(true);
+        return true;
+      }
+
+      if (!topDiv) {
+        return false;
+      }
+
+      const distanceFromBottom =
+        topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
+      return distanceFromBottom <= 100;
+    };
+
+    if (shouldAutoScroll()) {
+      setTimeout(() => {
+        bottomRef.current?.scrollIntoView({
+          behavior: "smooth",
+        });
+      }, 100);
+    }
+  }, [bottomRef, chatRef, count, hasInitialized]);
 };
